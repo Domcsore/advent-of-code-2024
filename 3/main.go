1623b2d8e0fc6d7877165ce71aa5a7c540e22e39
Dominic Sore <domcsore@googlemail.com>
2024-12-03 22:42:24 +0000

    day 3 part 2

--- 3/main.go
+++ 3/main.go
@@ -8,6 +8,72 @@ import (
 	"github.com/domcsore/advent-of-code-2024/util"
 )
 
+func GetMulCommands(b []byte) [][]byte {
+	mulRe := regexp.MustCompile(`mul\(\d{1,3},\d{1,3}\)`)
+	return mulRe.FindAll(b, -1)
+}
+
+func GetMulCommandMultiplicants(b []byte) (int, int, error) {
+	digRe := regexp.MustCompile(`\d{1,3}`)
+	multiplicants := digRe.FindAll(b, -1)
+
+	if len(multiplicants) != 2 {
+		return 0, 0, fmt.Errorf("mul commands must contain 2 numbers")
+	}
+
+	multiplicantOne, err := util.IntFromBytes(multiplicants[0])
+	if err != nil {
+		return 0, 0, err
+	}
+
+	multiplicantTwo, err := util.IntFromBytes(multiplicants[1])
+	if err != nil {
+		return 0, 0, err
+	}
+
+	return multiplicantOne, multiplicantTwo, nil
+}
+
+func CalculateAllMulInstructions(b []byte) int {
+	matches := GetMulCommands(b)
+	if matches == nil {
+		return 0
+	}
+
+	total := 0
+
+	for i := 0; i < len(matches); i++ {
+		multiplicantOne, multiplicantTwo, err := GetMulCommandMultiplicants(matches[i])
+		if err != nil {
+			panic(err)
+		}
+
+		total += multiplicantOne * multiplicantTwo
+	}
+
+	return total
+}
+
+func GetDoBlocks(b []byte) [][]byte {
+	doBlockRe := regexp.MustCompile(`(\A|do\(\))(.|\n)*?(don\'t\(\)|\z)`)
+
+	return doBlockRe.FindAll(b, -1)
+}
+
+func CalculateDoBlockInstructions(b []byte) int {
+	doBlocks := GetDoBlocks(b)
+
+	total := 0
+
+	for i := 0; i < len(doBlocks); i++ {
+		// fmt.Printf("%s\f", doBlocks[i])
+		blockTotal := CalculateAllMulInstructions(doBlocks[i])
+		total += blockTotal
+	}
+
+	return total
+}
+
 func main() {
 	inputReader := util.OpenFileFromArgs()
 
@@ -16,37 +82,9 @@ func main() {
 		panic(err)
 	}
 
-	mulRe := regexp.MustCompile(`mul\(\d{1,3},\d{1,3}\)`)
-
-	matches := mulRe.FindAll(input, -1)
-
-	if matches == nil {
-		fmt.Println("No matches found")
-		return
-	}
-
-	digRe := regexp.MustCompile(`\d{1,3}`)
-
-	total := 0
-
-	for i := 0; i < len(matches); i++ {
-		multiplicants := digRe.FindAll(matches[i], -1)
-		if len(multiplicants) != 2 {
-			panic("Need exactly two multiplicants")
-		}
-
-		multiplicantOne, err := util.IntFromBytes(multiplicants[0])
-		if err != nil {
-			panic(err)
-		}
-
-		multiplicantTwo, err := util.IntFromBytes(multiplicants[1])
-		if err != nil {
-			panic(err)
-		}
-
-		total += multiplicantOne * multiplicantTwo
-	}
-
+	total := CalculateAllMulInstructions(input)
 	fmt.Printf("Total - %d\n", total)
+
+	doBlockTotal := CalculateDoBlockInstructions(input)
+	fmt.Printf("Do Block Total - %d\n", doBlockTotal)
 }
