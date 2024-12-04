9e5628248fe1188736770cb802ec9d1a639b8c15
Dominic Sore <domcsore@googlemail.com>
2024-12-04 13:39:38 +0000

    day 4 part 2

--- 4/main.go
+++ 4/main.go
@@ -41,10 +41,15 @@ type WordSearch struct {
 	rows      int
 	columns   int
 	foundMask [][]bool
+	crossMask [][]bool
 }
 
-func (ws *WordSearch) Print() {
-	printWordSearch(ws.data, ws.foundMask)
+func (ws *WordSearch) Print(mask int) {
+	if mask == 1 {
+		printWordSearch(ws.data, ws.foundMask)
+	} else {
+		printWordSearch(ws.data, ws.crossMask)
+	}
 }
 
 func (ws *WordSearch) hasWordsAtPosition(row int, column int, word []rune) (foundCount int) {
@@ -211,6 +216,86 @@ func (ws *WordSearch) FindWords(word []rune) (foundCount int) {
 	return foundCount
 }
 
+func (ws *WordSearch) hasCrossMasAtPosition(row int, column int) (found bool) {
+	if row == 0 || row == (ws.rows-1) || column == 0 || (column == ws.columns-1) {
+		return false
+	}
+
+	if ws.data[row][column] != 'A' {
+		return false
+	}
+
+	// TL to BR
+	topLeftRune := ws.data[row-1][column-1]
+
+	if topLeftRune != 'M' && topLeftRune != 'S' {
+		return false
+	}
+
+	bottomRightRune := ws.data[row+1][column+1]
+
+	oppoTopLeftRune, err := OppositeSorM(topLeftRune)
+	if err != nil {
+		panic(err)
+	}
+
+	if bottomRightRune != oppoTopLeftRune {
+		return false
+	}
+
+	// TR to BL
+	topRightRune := ws.data[row-1][column+1]
+
+	if topRightRune != 'M' && topRightRune != 'S' {
+		return false
+	}
+
+	bottomLeftRune := ws.data[row+1][column-1]
+
+	oppoTopRightRune, err := OppositeSorM(topRightRune)
+	if err != nil {
+		panic(err)
+	}
+
+	if bottomLeftRune != oppoTopRightRune {
+		return false
+	}
+
+	ws.crossMask[row][column] = true
+	ws.crossMask[row+1][column+1] = true
+	ws.crossMask[row+1][column-1] = true
+	ws.crossMask[row-1][column-1] = true
+	ws.crossMask[row-1][column+1] = true
+
+	return true
+}
+
+func (ws *WordSearch) FindCrossMasWords() int {
+	foundCount := 0
+	for ri, r := range ws.data {
+		for ci := range r {
+			found := ws.hasCrossMasAtPosition(ri, ci)
+			if found {
+				foundCount++
+			}
+		}
+	}
+	return foundCount
+}
+
+func OppositeSorM(r rune) (rune, error) {
+	if r == 'M' {
+		return 'S', nil
+	}
+
+	if r == 'S' {
+		return 'M', nil
+	}
+
+	return 'A', fmt.Errorf("expected rune S or M, recieved %c", r)
+
+}
+
 func printWordSearch(ws [][]rune, mask [][]bool) {
 	for ri, r := range ws {
 		for ci, c := range r {
@@ -246,11 +331,17 @@ func CreateWordSearchFromFileReader(i *bufio.Reader) (*WordSearch, error) {
 		foundMask[i] = make([]bool, columns)
 	}
 
+	crossMask := make([][]bool, rows)
+	for i := 0; i < len(crossMask); i++ {
+		crossMask[i] = make([]bool, columns)
+	}
+
 	return &WordSearch{
 		data,
 		rows,
 		columns,
 		foundMask,
+		crossMask,
 	}, nil
 }
 
@@ -263,8 +354,8 @@ func main() {
 	}
 
 	wordsFound := ws.FindWords([]rune("XMAS"))
-
-	ws.Print()
-
 	fmt.Printf("Word found - %d\n", wordsFound)
+
+	crossCount := ws.FindCrossMasWords()
+	fmt.Printf("Cross Mas's found - %d\n", crossCount)
 }
